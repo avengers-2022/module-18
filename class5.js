645b8e7b7f06b2ba4886e434872d3fe41129b15f
programmerArif <arifulislamwasim2017@gmail.com>
2022-07-10 15:30:19 +0600

    class 5 done

---
+++ class5.js
@@ -0,0 +1,11 @@
+var computer = {
+    price: 29000,
+    storage: '250gb',
+    processor: 'i3 7th gen',
+    color: 'silver'
+}
+
+computer.ram = '4gb + 4gb'
+computer.storage = 'HDD-1TB + SSD-250GB'
+
+console.log(computer);
