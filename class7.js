946ccc1da5b7902c6f2f4b7fa65baef4c682ca29
programmerArif <arifulislamwasim2017@gmail.com>
2022-07-10 16:06:32 +0600

    class 7 done

---
+++ class7.js
@@ -0,0 +1,28 @@
+var color = 'black';
+
+// if (color == 'white') {
+//     console.log('This Color is White');
+// } else if (color == 'black') {
+//     console.log('This Color is Black');
+// } else if (color == 'cyan') {
+//     console.log('This Color is Cyan');
+// } else if (color == 'green') {
+//     console.log('This Color is Green');
+// }
+
+switch (color) {
+    case 'white':
+        console.log('This Color is White');
+        break;
+    case 'black':
+        console.log('This Color is black');
+        break;
+    case 'cyan':
+        console.log('This Color is cyan');
+        break;
+    case 'green':
+        console.log('This Color is green');
+        break;
+    default:
+        console.log('This Color is not match');
+}
